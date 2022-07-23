a05c4c378301d2df01993b21f1b006364410c7d7
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-07-23 23:08:43 +0900

    Jump

--- ShotingGame1/Assets/Scripts/Player/PlayerMovement.cs
+++ ShotingGame1/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5.0f;    //이동 속도
+    [SerializeField] float jumpForce = 3.0f;
+    float gravity = -9.81f;
     Vector3 moveDirection;                      //이동 방향
+    Vector3 lookDirection;                      //바라보는 방향
 
     CharacterController characterController;
 
@@ -16,6 +19,14 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+
+        if (characterController.isGrounded == false)        //바닥 충돌 체크 
+            moveDirection.y += gravity * Time.deltaTime;    //y축 감소
+
+        //점프 체크
+        if (Input.GetKeyDown(KeyCode.Space))
+            JumpTo();
+
         Move();
     } 
 
@@ -24,10 +35,23 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical"); 
 
-        moveDirection = new Vector3(x,0,z);
+        moveDirection =  MoveTo(new Vector3 (x, 0, z));
+        lookDirection = new Vector3 (x, 0, z);
 
-        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);   //이동
 
-        transform.rotation = Quaternion.LookRotation(moveDirection);
+        transform.rotation = Quaternion.LookRotation(lookDirection);            //이동 방향으로 회전
+    }
+
+    Vector3 MoveTo(Vector3 direction)
+    {
+        return new Vector3(direction.x, moveDirection.y, direction.z);         //방향 벡터 구하기
+
+    }
+
+    void JumpTo()
+    {
+        if (characterController.isGrounded == true)
+            moveDirection.y = jumpForce;
     }
 }
