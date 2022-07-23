80401ad7b0c01c64bb7fab79119e32e21835a460
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-07-23 21:43:51 +0900

    player Move

--- ShotingGame1/Assets/Scripts/Player/PlayerMovement.cs
+++ ShotingGame1/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float moveSpeed = 5.0f;    //이동 속도
+    Vector3 moveDirection;                      //이동 방향
+
+    CharacterController characterController;
+
+    private void Awake()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical"); 
+
+        MoveTo(new Vector3(x,0,z));
+    }
+
+    void MoveTo(Vector3 direction)
+    {
+        moveDirection = direction;
     }
 }
