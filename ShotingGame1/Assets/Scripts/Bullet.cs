0660d36c5f5b5bc9f5b89ebe76248474a2ac51db
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-07-24 12:57:51 +0900

    Bullet

--- ShotingGame1/Assets/Scripts/Bullet.cs
+++ ShotingGame1/Assets/Scripts/Bullet.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    bool isFire;
+    Vector3 direction;      //총알이 날아가는 방향
+    float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject,3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFire) //발사처리가 된 후
+        {
+            transform.Translate(direction * Time.deltaTime * speed);
+        }
+    }
+
+    public void Fire(Vector3 dir)
+    {
+        direction = dir;
+        isFire = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Bullet>() == null)
+            Destroy(gameObject);
     }
 }
